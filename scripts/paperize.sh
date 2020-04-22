6dbae3f9a341477c4acf4e1b1b4a3ebba6a2494e
Marc Steiner <astroviking@protonmail.com>
2020-04-22 01:21:50 +0000

    Use pandoc-plot.

--- scripts/paperize.sh
+++ scripts/paperize.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -14,4 +14,4 @@ TEXLIVE_BIN_DIR="$TEXLIVE_DIR/2020/bin/x86_64-linux/"
 $DIR/install-tools.sh
 
 jupyter nbconvert --to markdown $DIR/../thesis.ipynb
-export PATH=$PATH:$TEXLIVE_BIN_DIR && pandoc -s $DIR/../thesis.md -t pdf -o $DIR/../thesis.pdf --filter pandoc-crossref --filter pandoc-citeproc --bibliography=citations.bib --csl=$DIR/../citation-styles/aip.csl -M date="$(date "+%B %e, %Y")" --template $DIR/../templates/eisvogel-m --listings --number-sections --highlight-style pygments
+export PATH=$PATH:$TEXLIVE_BIN_DIR && pandoc -f markdown -s $DIR/../thesis.md -t pdf -o $DIR/../thesis.pdf --filter pandoc-plot --filter pandoc-crossref --filter pandoc-citeproc --bibliography=citations.bib --csl=$DIR/../citation-styles/aip.csl -M date="$(date "+%B %e, %Y")" --template $DIR/../templates/eisvogel-m --listings --number-sections --highlight-style breezedark
