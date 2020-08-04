314ac27ebe66c4c6cbb2db86e8a18b27e50d57a7
Marc Steiner <astroviking@protonmail.com>
2020-08-05 01:48:56 +0200

    Hopefully fix virtual env creation once and for all...

--- scripts/createVirtualEnv.R
+++ scripts/createVirtualEnv.R
@@ -1,12 +1,13 @@
 library(devtools)
+devtools::install_github("rstudio/tensorflow")
 devtools::install_github("rstudio/reticulate")
+
+# Install tensorflow with R to avoid crashes of RStudio
+library(tensorflow)
+install_tensorflow(method = "conda", envname = "ba-thesis")
 
 # Create conda env ba-thesis and install tf-kde
 library(reticulate)
 conda_create('ba-thesis')
 conda_install('ba-thesis', 'pip')
 conda_install('ba-thesis', '-e src/tf-kde', pip = TRUE)
-
-# Reinstall tensorflow with R to avoid crashes of RStudio
-library(tensorflow)
-install_tensorflow(method = "conda", envname = "ba-thesis")
