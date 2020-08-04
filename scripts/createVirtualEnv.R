cdb831713e23d0bdd1161329d9878cbd3dd886e5
Marc Steiner <astroviking@protonmail.com>
2020-08-05 01:17:33 +0200

    Update createVirtualEnv script.

--- scripts/createVirtualEnv.R
+++ scripts/createVirtualEnv.R
@@ -1,3 +1,12 @@
+library(devtools)
+devtools::install_github("rstudio/reticulate")
+
+# Create conda env ba-thesis and install tf-kde
 library(reticulate)
 conda_create('ba-thesis')
+conda_install('ba-thesis', 'pip')
 conda_install('ba-thesis', '-e src/tf-kde', pip = TRUE)
+
+# Reinstall tensorflow with R to avoid crashes of RStudio
+library(tensorflow)
+install_tensorflow(method = "conda", envname = "ba-thesis")
